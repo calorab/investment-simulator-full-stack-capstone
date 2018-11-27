910c5c86b5937393e235da08ae8f103fe973727b
Caleb Gammon <calebwgammon@gmail.com>
2018-11-27 12:23:56 -0500

    testing

--- public/client.js
+++ public/client.js
@@ -409,7 +409,7 @@ $(document).on("click", '.createNewInvestment', function (event) {
     //if the input is valid
     else {
         createInvestment(investmentSymbol, portfolioId);
-        document.getElementById("#contactForm").reset();
+        document.getElementById(".stockSearch").reset();
     };
 });
 
